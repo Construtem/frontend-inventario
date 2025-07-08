dae369374c8a786006ac884c6dcf651f30be0fd0
cris <87342708+DERCHITO@users.noreply.github.com>
2025-07-07 23:48:30 -0400

    fix reverse

--- src/app/admin/sucursales/page.tsx
+++ src/app/admin/sucursales/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { FaSearch } from "react-icons/fa";
+// import { FaSearch } from "react-icons/fa";
+
 
 interface TipoSucursal {
   id: number;
